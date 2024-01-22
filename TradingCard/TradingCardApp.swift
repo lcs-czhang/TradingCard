5ae27545ad4e38c32bf9c77cc2f992c0ef4a49a0
Youzhou Zhang <czhang@lcs.on.ca>
2024-01-22 09:06:19 -0500

    Initial Commit

---
+++ TradingCard/TradingCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardApp.swift
+//  TradingCard
+//
+//  Created by Yuzhou Zhang on 2024-01-22.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
