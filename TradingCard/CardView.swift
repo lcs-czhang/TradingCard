dd496c96bbeb7fb6ead847f9f78dfa04b8b8a0c8
Youzhou Zhang <czhang@lcs.on.ca>
2024-01-25 13:53:26 -0500

    update image and add another card

--- TradingCard/CardView.swift
+++ TradingCard/CardView.swift
@@ -15,38 +15,56 @@ struct CardView: View {
     let characterHitPoint: Int
     var body: some View {
         VStack {
-            Rectangle()
-                .frame(width: 350.0, height: 435.0)
-                .foregroundStyle(.black)
-                .overlay(VStack {
-                    Spacer()
-                    Rectangle()
-                        .foregroundStyle(.yellow)
-                        .frame(width: 300.0, height: 75.0)
-                        .overlay(HStack {
-                            Image(forceName)
-                                .resizable()
-                                .scaledToFit()
-                            Text(characterName)
-                                .font(.title)
-                                .foregroundColor(Color.white)
-                            Image("gouyu")
-                                .resizable()
-                            Text("X\(characterHitPoint)")
-                                .font(.title)
-                                .foregroundColor(Color.white)
-                        })
-                    Image(imageName)
-                        .resizable()
-                        .frame(width: 300.0, height: 300.0)
-                        .padding(.bottom, 25.0)
+            ZStack {
+                Image("武将背景")
+                    .resizable()
+                    .ignoresSafeArea()
+                    .scaledToFill()
+                    .overlay(VStack {
+                        Rectangle()
+                            .foregroundStyle(.black)
+                            .opacity(0.5)
+                            .frame(width: 300.0, height: 60.0)
+                            .overlay(HStack {
+                                Image(forceName)
+                                    .resizable()
+                                    .scaledToFit()
+                                Text(characterName)
+                                    .font(.title)
+                                    .foregroundColor(Color.white)
+                                Image("gouyu")
+                                    .resizable()
+                                    .scaledToFit()
+                                Text("X\(characterHitPoint)")
+                                    .font(.title)
+                                    .foregroundColor(Color.white)
+                                    .padding(.trailing, 10.0)
+                            })
+                        Image(imageName)
+                            .resizable()
+                            .frame(width: 300.0, height: 300.0)
+                        ZStack {
+                            Rectangle()
+                                .opacity(0.5)
+                            ScrollView(.vertical){
+                                Text(characterDescription)
+                                    .font(.callout)
+                                    .foregroundStyle(.white)
+                                    .padding(25.0)
+                            }
+                            
+                        }
+                        .frame(width: 300.0)
                 })
-            Text(characterDescription)
+            }
         }
             
     }
 }
 
 #Preview {
-    CardView(imageName: "jiedabao", forceName: "wu", characterName: "界徐盛", characterDescription: "", characterHitPoint: 4)
+    CardView(imageName: "jiedabao", forceName: "wu", characterName: "界徐盛", characterDescription: """
+             破军
+             当你使用【杀】指定一个目标后，你可以发动此技能，将其至多X张牌扣置于该角色的武将牌旁（X为其体力值）；若如此做，当前回合结束后，该角色获得这些牌。你使用【杀】对手牌数与装备数均不大于你的角色造成伤害时，此伤害+1。
+             """, characterHitPoint: 4)
 }
